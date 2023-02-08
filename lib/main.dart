e577cb45b213e2e25c1c7c4c0fda4c77fc211e33
akhmadsyarif04 <akhmadsyarif04.arif@gmail.com>
2023-02-08 23:25:19 +0800

    Adding the 'Add place' Screen & An Image Input

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:great_places_app/providers/great_places.dart';
 import 'package:provider/provider.dart';
 
 import './screens/places_list_screen.dart';
+import './screens/add_place_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,7 +18,10 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.indigo,
             accentColor: Colors.amber,
           ),
-          home: PlacesListScreen()),
+          home: PlacesListScreen(),
+          routes: {
+            AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+          }),
     );
   }
 }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import './add_place_screen.dart';
+
 class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Places'),
-        actions: <Widget>[IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+              },
+              icon: Icon(Icons.add))
+        ],
       ),
       body: Center(
         child: CircularProgressIndicator(),
