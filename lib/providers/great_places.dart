9620be173c3a0fca1afd56c09756b409c8f33934
akhmadsyarif04 <akhmadsyarif04.arif@gmail.com>
2023-02-09 20:19:50 +0800

    Preparing SQLite

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -18,5 +19,10 @@ class GreatPlaces with ChangeNotifier {
         image: pickedImage);
     _items.add(newPlace);
     notifyListeners();
+    DBHelper.insert('places', {
+      'id': newPlace.id!,
+      'title': newPlace.title!,
+      'image': newPlace.image!.path
+    });
   }
 }
